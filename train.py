4f85e9327207e0fc372d971bb6b6a16a31932e35
love-whut <71579899+love-whut@users.noreply.github.com>
2023-12-18 20:15:50 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('ultralytics/cfg/extra_models/rtdetr-CascadedGroupAttention-Attention(full).yaml')
+    model = RTDETR('ultralytics/cfg/models/rtdetr-r18.yaml')
     # model.load('') # loading pretrain weights
     model.train(data='dataset/NWPU.yaml',
                 cache=False,
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                 epochs=200,
                 batch=4,
                 workers=4,
-                device='3',
+                device='0',
                 # resume='', # last.pt path
                 project='runs/train',
                 name='exp',
